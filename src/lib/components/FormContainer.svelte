7b5f19f93b2be3405963c3af2c11032b1c93e187
t63065488 <t63065488@gmail.com>
2024-01-17 21:07:46 +0000

    Add base form component, remove helloworld component

---
+++ src/lib/components/FormContainer.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import RandomScore from "$lib/components/ability-score/RandomScores.svelte"
+    import PointBuy from "$lib/components/ability-score/PointBuy.svelte"
+</script>
+
+<form></form>
