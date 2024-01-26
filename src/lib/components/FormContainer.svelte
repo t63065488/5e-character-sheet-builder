dea7ff2d636d2d059157c7617908654a54585a20
t63065488 <t63065488@gmail.com>
2024-01-26 15:36:52 +0000

    Made several changes to structrure. Form stepper component removed in favour of Skeleton stepper component. Added flowbite dependencies for icons/formatting. Updated main page to use tailwind css and have some vague formatting for the form.

--- src/lib/components/FormContainer.svelte
+++ src/lib/components/FormContainer.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
     import PointBuy from "$lib/components/ability-score/PointBuy.svelte"
     import { Race } from "$lib/types/race";
-    import FormStepper from "./FormStepper.svelte";
+    import AbilityScorePicker from "./ability-score/AbilityScorePicker.svelte";
     import CharacterInfo from "./character-info/CharacterInfo.svelte";
+    import {Stepper, Step } from '@skeletonlabs/skeleton'
 </script>
 
-<div class="w-6/12">
-    <FormStepper/>
-</div>
-<div class="w-6/12">
-    <CharacterInfo races={[new Race("Human", "6ft", 30)]}/>
-    <PointBuy/>
+<div class="w-full">
+    <Stepper>
+        <Step>
+            <svelte:fragment slot="header">Character Info</svelte:fragment>
+            <CharacterInfo races={[new Race("Human", "6ft", 30)]}/>
+        </Step>
+        <Step>
+            <svelte:fragment slot="header">Ability Scores</svelte:fragment>
+            <AbilityScorePicker/>
+        </Step>
+    </Stepper>
 </div>
---
+++ src/lib/components/ability-score/AbilityScorePicker.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { TabGroup, Tab } from "@skeletonlabs/skeleton";
+    import PointBuy from "./PointBuy.svelte";
+    import RandomScores from "./RandomScores.svelte";
+
+    let currentTab: number = 0;
+</script>
+
+<div>
+    <TabGroup justify="justify-center">
+        <!-- Tab Definitions -->
+        <Tab bind:group={currentTab} name="tab1" value={0}>
+            <span>Point Buy</span>
+        </Tab>
+        <Tab bind:group={currentTab} name="tab2" value={1}>
+            <span>Random</span>
+        </Tab>
+        <!-- Tab Content Definitions -->
+        <svelte:fragment slot="panel">
+            {#if currentTab === 0}
+                <PointBuy/>
+            {:else if currentTab === 1}
+                <RandomScores/>
+            {/if}
+        </svelte:fragment>
+    </TabGroup>
+</div>
