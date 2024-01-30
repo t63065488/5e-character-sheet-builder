5427dcbee08219affd473f7a3f401c8d4990413c
t63065488 <t63065488@gmail.com>
2024-01-30 19:21:06 +0000

    Convert Race to interface

--- src/lib/types/race.ts
+++ src/lib/types/race.ts
@@ -1,13 +1,7 @@
-export class Race {
-  constructor(
-    private name: string,
-    private size: string,
-    private speed: number,
-    private features: any[],
-    private heightRange?: string,
-  ) {}
-
-  public getName(): string {
-    return this.name;
-  }
+export interface Race {
+    name: string,
+    size: string,
+    speed: number,
+    features: any[],
+    heightRange?: string,
 }
