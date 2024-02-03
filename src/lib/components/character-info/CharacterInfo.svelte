e0cc622e8abb93df2c78a32482cf108a8df0ec34
t63065488 <t63065488@gmail.com>
2024-02-03 13:51:39 +0000

    Bind value to store value rather than using function

--- src/lib/components/character-info/CharacterInfo.svelte
+++ src/lib/components/character-info/CharacterInfo.svelte
@@ -3,44 +3,16 @@
   import { characterInfoStore } from "$lib/stores";
 
   export let races: Race[] = [];
-
-  let selectedRace: Race;
-  let characterName: string = "";
-
-  const onEnterName = () => {
-    console.log("Character name update!")
-    characterInfoStore.update((character) => {
-      return {
-        abilityBlocks: character.abilityBlocks, 
-        characterInfo: {
-          ...character.characterInfo,
-          name: characterName
-        }
-      }
-    })
-  }
-
-  const onSelectRace = () => {
-    characterInfoStore.update((character) => {
-      return {
-        abilityBlocks: character.abilityBlocks, 
-        characterInfo: {
-          ...character.characterInfo,
-          race: selectedRace
-        }
-      }
-    })
-  }
 </script>
 
 <div class="flex w-full">
   <label class="label">
     <span>Character Name</span>
-    <input class="input" type="text" bind:value={characterName} on:input={onEnterName}/>
+    <input class="input" type="text" bind:value={$characterInfoStore.characterInfo.name}/>
   </label>
   <label class="label">
     <span>Race</span>
-    <select class="select" bind:value={selectedRace} on:change={onSelectRace}>
+    <select class="select" bind:value={$characterInfoStore.characterInfo.race}>
       {#each races as race}
         <option>{race.name}</option>
       {/each}
