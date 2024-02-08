ff144b5f26c8ab697feb7ef027658061d73d2238
t63065488 <t63065488@gmail.com>
2024-02-08 21:20:01 +0000

    Minor ability score container refactor, comments, ability bonus component

---
+++ src/lib/components/ability-score/AbilityScoreBonuses.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { AbilityBonus } from "$lib/types/race";
+
+    export let abilityBonuses: AbilityBonus[] = [];
+</script>
+
+<!-- 
+    @component
+    Enables application of bonuses to stats. If a stat bonus provides a designated stat, these will be displayed. Otherwise the stat can be selected.
+    If there are no bonuses, this block will render nothing.
+ -->
+
+{#each abilityBonuses as bonus}
+ <p>{bonus.abilityType} + {bonus.bonus}</p>
+{/each}
+
---
+++ src/lib/components/ability-score/AbilityScoreParentContainer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import AbilityScoreBonuses from "./AbilityScoreBonuses.svelte";
+import AbilityScorePicker from "./AbilityScorePicker.svelte";
+import { characterInfoStore } from "$lib/stores";
+    import AbilityContainer from "./AbilityContainer.svelte";
+
+</script>
+
+<!-- 
+    @component
+    Parent container for ability scores. This component uses 3 child components- a picker (e.g. random, point buy), racial bonuses (either statically applied, or selected) and the stat blocks themselves.
+
+    The picker determines the initial base stats. The racial bonuses are then applied and the final result is displayed in the blocks.
+ -->
+
+ <div>
+    <AbilityScorePicker />
+    <hr/>
+    <AbilityScoreBonuses abilityBonuses={$characterInfoStore.characterInfo.race?.abilityBonuses ?? []} />
+    <hr/>
+    <AbilityContainer abilityBlocks={$characterInfoStore.abilityBlocks}/>
+ </div>
