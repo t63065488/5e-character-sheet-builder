98cb6a57d88ab3a8ca591f93c2c062be707ef5dd
t63065488 <t63065488@gmail.com>
2024-02-13 21:01:55 +0000

    #1 run format, more test

--- tests/lib/types/abilityScore.test.ts
+++ tests/lib/types/abilityScore.test.ts
@@ -1,18 +1,33 @@
-import { expect, test } from "vitest"
-import { AbilityScore, getAbilityScore, getDeafultAbilityScores } from "$lib/types/abilityScore";
+import { expect, test } from "vitest";
+import {
+  AbilityScore,
+  getAbilityScore,
+  getDeafultAbilityScores,
+} from "$lib/types/abilityScore";
 import AbilityType from "$lib/types/abilityType";
 
-test('getAbilityScore', () => {
-    // Given
-    const expectedResult: AbilityScore =         {
-        abilityType: AbilityType.CHA,
-        baseScore: 16,
-        bonusScore: 0,
-        totalScore: 16,
-    }
-    // When
-    const result = getAbilityScore(16, 0, AbilityType.CHA);
-    // Then
-    expect(result).not.toBeNull();
-    expect(result).toEqual(expectedResult);
-})
+test("getAbilityScore", () => {
+  // Given
+  const expectedResult: AbilityScore = {
+    abilityType: AbilityType.CHA,
+    baseScore: 16,
+    bonusScore: 0,
+    totalScore: 16,
+  };
+  // When
+  const result = getAbilityScore(16, 0, AbilityType.CHA);
+  // Then
+  expect(result).not.toBeNull();
+  expect(result).toEqual(expectedResult);
+});
+
+test("getDefaultAbilityScores", () => {
+  // Given
+  const defaultScore: number = 20;
+  // When
+  const result: AbilityScore[] = getDeafultAbilityScores(defaultScore);
+  // Then
+  result.forEach((result) => {
+    expect(result.baseScore).toBe(defaultScore);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [sveltekit()],
   test: {
     environment: "jsdom",
-    include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    exclude: [...configDefaults.exclude, './*.config.{js,ts,cjs}']
+    include: ["./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: [...configDefaults.exclude, "./*.config.{js,ts,cjs}"],
   },
 });
