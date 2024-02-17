f004b3b8e649782985225a9d29cb691289708710
t63065488 <t63065488@gmail.com>
2024-02-17 17:25:10 +0000

    #5 Refactor AbilityContainer to single AbilityScore, better formatting

--- src/lib/components/ability-score/AbilityContainer.svelte
+++ src/lib/components/ability-score/AbilityContainer.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
   import { AbilityScore } from "$lib/types/abilityScore";
+    import { calculateModifier } from "$lib/utils/modifiers";
 
-  export let abilityBlocks: AbilityScore[];
+  export let abilityScore: AbilityScore;
 </script>
 
-<ul id="score-list">
-  {#each abilityBlocks as block}{/each}
-</ul>
+<style>
+
+</style>
+
+<!-- @component
+  Rounded card to display an AbilityScore and modifiers.
+  Usage:
+  <AbilityContainer abilityScore={block}/>
+ -->
+ <div class="card text-center h-20">
+    <h3 class="h3">{abilityScore.abilityType}</h3>
+    <h3 class="h3">{abilityScore.totalScore}</h3>
+    <div class="rounded">
+      <h4 class="h4">{calculateModifier(abilityScore.totalScore)}</h4>
+    </div>
+ </div>
