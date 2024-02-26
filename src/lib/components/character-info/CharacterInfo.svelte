5d4cfc1e2c92b4a763420fa592f0818c7f1f7579
t63065488 <t63065488@gmail.com>
2024-02-26 20:30:07 +0000

    #9 Initial function

--- src/lib/components/character-info/CharacterInfo.svelte
+++ src/lib/components/character-info/CharacterInfo.svelte
@@ -3,6 +3,22 @@
   import { characterInfoStore } from "$lib/stores";
 
   export let races: Race[] = [];
+
+  const handleSelectRace = () => {
+    characterInfoStore.update(character => {
+      character.characterInfo.race?.abilityBonuses?.forEach(element => {
+        character.abilityScores.filter(score => score.abilityType === element.abilityType).map((score) => {
+          score.bonusScore = element.bonus
+          score.totalScore = score.baseScore + score.bonusScore
+          return score;
+        })
+      });
+      console.log(character)
+      return {
+        ...character,
+      }
+    })
+  }
 </script>
 
 <div class="flex w-full">
@@ -16,7 +32,7 @@
   </label>
   <label class="label">
     <span>Race</span>
-    <select class="select" bind:value={$characterInfoStore.characterInfo.race}>
+    <select class="select" bind:value={$characterInfoStore.characterInfo.race} on:change={handleSelectRace}>
       {#each races as race}
         <option value={race}>{race.name}</option>
       {/each}
