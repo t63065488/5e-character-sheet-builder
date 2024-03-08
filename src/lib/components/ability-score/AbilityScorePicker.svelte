bd145b353ef8d7619e1dca5dbf7bd73e01de83e0
t63065488 <t63065488@gmail.com>
2024-03-08 19:14:58 +0000

    #13 initial plan

--- src/lib/components/ability-score/AbilityScorePicker.svelte
+++ src/lib/components/ability-score/AbilityScorePicker.svelte
@@ -4,15 +4,18 @@
   import RandomScores from "./RandomScores.svelte";
 
   let currentTab: number = 0;
+
+  // When a tab is selected, the scores in that tab should be used over the previous tabs. 
+  // Scores should remain unique to the tab, rather than applying globally to the store.
 </script>
 
 <div>
   <TabGroup justify="justify-center">
     <!-- Tab Definitions -->
-    <Tab bind:group={currentTab} name="tab1" value={0}>
+    <Tab bind:group={currentTab} name="tab1" value={0} on:click={() => {}}>
       <span>Point Buy</span>
     </Tab>
-    <Tab bind:group={currentTab} name="tab2" value={1}>
+    <Tab bind:group={currentTab} name="tab2" value={1} on:click={() => {}}>
       <span>Random</span>
     </Tab>
     <!-- Tab Content Definitions -->
