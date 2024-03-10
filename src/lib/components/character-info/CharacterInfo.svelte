ec6594418067afca586a1381cc1617f7062756a6
t63065488 <t63065488@gmail.com>
2024-03-10 18:06:34 +0000

    #13 Adjust racial selection to display racial bonuses

--- src/lib/components/character-info/CharacterInfo.svelte
+++ src/lib/components/character-info/CharacterInfo.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import { Race } from "$lib/types/race";
   import { characterStore, updateCharacterRace } from "$lib/characterStore";
+  import {
+    Accordion,
+    AccordionItem,
+    ListBox,
+    ListBoxItem,
+  } from "@skeletonlabs/skeleton";
+  import RaceInfo from "./RaceInfo.svelte";
 
   export let races: Race[] = [];
-
-  let selectedRace: Race;
 </script>
 
-<div class="flex w-full">
+<div class="p-4">
   <label class="label">
     <span>Character Name</span>
     <input
@@ -16,16 +21,29 @@
       bind:value={$characterStore.characterInfo.name}
     />
   </label>
-  <label class="label">
-    <span>Race</span>
-    <select
-      class="select"
-      bind:value={selectedRace}
-      on:change={() => updateCharacterRace(selectedRace)}
-    >
-      {#each races as race}
-        <option value={race}>{race.name}</option>
-      {/each}
-    </select>
-  </label>
+  <div class="p-4" />
+  <hr />
+  <div class="p-4" />
+
+  <h2 class="step-header text-2xl font-bold">Selected Race</h2>
+  {#if $characterStore.characterInfo.race === undefined}
+    <h2>Select a race below...</h2>
+  {:else}
+    <h2>{$characterStore.characterInfo.race.name}</h2>
+    <RaceInfo race={$characterStore.characterInfo.race} selectEnabled={false} />
+  {/if}
+  <div class="p-4" />
+  <hr />
+  <div class="p-4" />
+  <h2 class="step-header text-2xl font-bold">Available Races</h2>
+  <Accordion>
+    {#each races as race}
+      <AccordionItem>
+        <svelte:fragment slot="summary">{race.name}</svelte:fragment>
+        <svelte:fragment slot="content"
+          ><RaceInfo {race} selectEnabled={true} /></svelte:fragment
+        >
+      </AccordionItem>
+    {/each}
+  </Accordion>
 </div>
---
+++ src/lib/components/character-info/RaceInfo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { updateCharacterRace } from "$lib/characterStore";
+  import { Race } from "$lib/types/race";
+
+  export let race: Race;
+  export let selectEnabled: boolean;
+</script>
+
+<div>
+  {#each race.features as feature}
+    <p>{feature.name}</p>
+  {/each}
+  {#each race.abilityBonuses || [] as bonus}
+    <p>{bonus.abilityType}:{bonus.bonusScore}</p>
+  {/each}
+  {#if selectEnabled}
+    <button
+      type="button"
+      class="btn btn-sm variant-filled"
+      on:click={() => updateCharacterRace(race)}>Select</button
+    >
+  {/if}
+</div>
