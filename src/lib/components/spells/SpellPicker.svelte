6fb9e9c26e91e84a19ddcf973ea74aba263e3675
t63065488 <t63065488@gmail.com>
2024-04-09 18:21:01 +0100

    #19 Refactor spell picker to use await

--- src/lib/components/spells/SpellPicker.svelte
+++ src/lib/components/spells/SpellPicker.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
-  import { loadSpell, spellStore } from "$lib/stores/spellStore";
   import {
     Accordion,
     AccordionItem,
     ProgressRadial,
   } from "@skeletonlabs/skeleton";
   import SpellInfo from "./SpellInfo.svelte";
-
-  const getSpellDefinition = (name: string, endpointUrl: string) => {
-    loadSpell(name, endpointUrl);
-  };
+  import { getSpell, getSpellEndpoints } from "$lib/utils/dndApi";
 </script>
 
-<Accordion autocollapse>
-  {#each $spellStore.spellEndpoints as endpoint}
-    <AccordionItem
-      on:click={() => getSpellDefinition(endpoint.name, endpoint.url)}
-    >
-      <svelte:fragment slot="summary">{endpoint.name}</svelte:fragment>
-      <svelte:fragment slot="content">
-        {#if $spellStore.spells[endpoint.name]}
-          <SpellInfo
-            spell={$spellStore.spells[endpoint.name]}
-            selectEnabled={true}
-          />
-        {:else}
-          <ProgressRadial value={undefined} />
-        {/if}
-      </svelte:fragment>
-    </AccordionItem>
-  {/each}
-</Accordion>
+{#await getSpellEndpoints()}
+  <ProgressRadial value={undefined} />
+  <p>Loading spells...</p>
+{:then spellEndpoints}
+  <Accordion autocollapse>
+    {#each spellEndpoints as endpoint}
+      <AccordionItem>
+        <svelte:fragment slot="summary">{endpoint.name}</svelte:fragment>
+        <svelte:fragment slot="content">
+          {#await getSpell(endpoint.url)}
+            <ProgressRadial value={undefined} />
+            <p>Loading {endpoint.name} definition...</p>
+          {:then spell}
+            <SpellInfo {spell} selectEnabled={true} />
+          {:catch error}
+            <p>
+              There was an error loading definition for spell {endpoint.name}... {error}
+            </p>
+          {/await}
+        </svelte:fragment>
+      </AccordionItem>
+    {/each}
+  </Accordion>
+{:catch error}
+  <p>There was an error loadding spells... {error}</p>
+{/await}
