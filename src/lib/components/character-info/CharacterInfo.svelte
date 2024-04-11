5645e511d556e78b23e49059dbf0537dceb87b2a
t63065488 <t63065488@gmail.com>
2024-04-11 18:13:29 +0100

    #19 Move race picker into its own component

--- src/lib/components/character-info/CharacterInfo.svelte
+++ src/lib/components/character-info/CharacterInfo.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
   import { characterStore } from "$lib/characterStore";
-  import {
-    Accordion,
-    AccordionItem,
-    ProgressRadial,
-  } from "@skeletonlabs/skeleton";
   import RaceInfo from "./RaceInfo.svelte";
-  import { getRace, getRaceEndpoints } from "$lib/utils/dndApi";
+  import RacePicker from "./RacePicker.svelte";
 </script>
 
 <div class="p-4">
@@ -33,30 +28,5 @@
   <hr />
   <div class="p-4" />
   <h2 class="step-header text-2xl font-bold">Available Races</h2>
-  {#await getRaceEndpoints()}
-    <ProgressRadial value={undefined} />
-    <p>Loading Races...</p>
-  {:then raceEndpoints}
-    <Accordion>
-      {#each raceEndpoints as raceEndpoint}
-        <AccordionItem>
-          <svelte:fragment slot="summary">{raceEndpoint.name}</svelte:fragment>
-          <svelte:fragment slot="content">
-            {#await getRace(raceEndpoint.url)}
-              <ProgressRadial value={undefined} />
-              <p>Loading {raceEndpoint.name}...</p>
-            {:then retrievedRace}
-              <RaceInfo race={retrievedRace} selectEnabled={true} />
-            {:catch error}
-              <p>
-                There was an error loading details for {raceEndpoint.name} - {error}
-              </p>
-            {/await}
-          </svelte:fragment>
-        </AccordionItem>
-      {/each}
-    </Accordion>
-  {:catch}
-    <p>There was an error loading available races...</p>
-  {/await}
+  <RacePicker />
 </div>
---
+++ src/lib/components/character-info/RacePicker.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { getRace, getRaceEndpoints } from "$lib/utils/dndApi";
+  import {
+    Accordion,
+    AccordionItem,
+    ProgressRadial,
+  } from "@skeletonlabs/skeleton";
+  import RaceInfo from "./RaceInfo.svelte";
+</script>
+
+{#await getRaceEndpoints()}
+  <ProgressRadial value={undefined} />
+  <p>Loading Races...</p>
+{:then raceEndpoints}
+  <Accordion>
+    {#each raceEndpoints as raceEndpoint}
+      <AccordionItem>
+        <svelte:fragment slot="summary">{raceEndpoint.name}</svelte:fragment>
+        <svelte:fragment slot="content">
+          {#await getRace(raceEndpoint.url)}
+            <ProgressRadial value={undefined} />
+            <p>Loading {raceEndpoint.name}...</p>
+          {:then retrievedRace}
+            <RaceInfo race={retrievedRace} selectEnabled={true} />
+          {:catch error}
+            <p>
+              There was an error loading details for {raceEndpoint.name} - {error}
+            </p>
+          {/await}
+        </svelte:fragment>
+      </AccordionItem>
+    {/each}
+  </Accordion>
+{:catch}
+  <p>There was an error loading available races...</p>
+{/await}
